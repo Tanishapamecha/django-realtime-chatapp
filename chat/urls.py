5533a7608beed6f795c9642972e5c1374bddc5b3
Tanisha <tanisha22pamecha@gmail.com>
2025-06-18 15:29:12 +0530

    previous message API

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from . import views
+from . import views 
+from .views import get_previous_messages
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('join/', views.join_room, name='join_room'),
     path('<str:room_name>/', views.room, name='room'),
-
+    path('messages/<str:room_name>/', get_previous_messages, name='get_previous_messages'),
 ]  
--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.models import User
-from .models import Profile
-
+from .models import Profile, Message
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from django.views.decorators.csrf import csrf_exempt
+from .serializers import MessageSerializer 
 
 def home(request):
     return render(request, 'chat/home.html')
+
 
 def room(request, room_name):
     username = request.GET.get('username', 'Guest')
@@ -20,3 +27,10 @@ def join_room(request):
     room_name = request.POST.get('room_name')
     return redirect(f'/chat/{room_name}/?username={username}')
 
+
+@api_view(['GET'])
+def get_previous_messages(request, room_name):
+    messages = Message.objects.filter(room=room_name).order_by('-timestamp')[:25]
+    messages = reversed(messages)  
+    serializer = MessageSerializer(messages, many=True)
+    return Response(serializer.data)
